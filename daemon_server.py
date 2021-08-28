0af43d7d0d84a439a91c4889d9fa4dc4fcfa9270
node1 <kayuaolequesp@gmail.com>
2021-08-28 19:31:10 -0300

    version

--- daemon_server.py
+++ daemon_server.py
@@ -235,6 +235,8 @@ class DaemonServer(Daemon):
 
                     if self.set_zookeeper_signal_sync():
 
+                        self.refresh_state_server()
+                        time.sleep(5)
                         datetime_now, list_registered_servers, list_registered_clients = self.get_state_monitor()
                         self.write_database(datetime_now, list_registered_servers, list_registered_clients)
 
@@ -287,6 +289,8 @@ class DaemonServer(Daemon):
 
                     if self.get_zookeeper_signal_sync():
 
+                        self.refresh_state_server()
+                        time.sleep(5)
                         datetime_now, list_registered_servers, list_registered_clients = self.get_state_monitor()
                         self.write_database(datetime_now, list_registered_servers, list_registered_clients)
 
